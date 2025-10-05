675f2a402fb638e07614d43eaf9e00688e128708
Mishka Tiwari <154558159+mishhkaaa@users.noreply.github.com>
2025-10-05 10:41:11 +0530

    Delete nasa directory

--- nasa/dashboard/components/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Components module initialization
-"""
-
-# This file makes the components directory a Python package
--- nasa/dashboard/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Utility module initialization
-"""
-
-# This file makes the utils directory a Python package
